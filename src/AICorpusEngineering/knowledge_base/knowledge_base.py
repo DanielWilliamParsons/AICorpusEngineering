183d9a385bf11e0f09db2c1eb1547cb26203c43c
Daniel Parsons <parsonsdwp@gmail.com>
2025-09-23 16:23:11 +0900

    feat: add knowledge base class knowledge creation method with category mappings included

--- src/AICorpusEngineering/knowledge_base/knowledge_base.py
+++ src/AICorpusEngineering/knowledge_base/knowledge_base.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+import json
 
 class KnowledgeBase:
     """
@@ -6,6 +8,37 @@ class KnowledgeBase:
     """
     def __init__(self):
         print("Initialize the knowledge base")
+        self.knowledge_base = ""
+        self.knowledge_base_mappings = {}
 
-    def create_knowledge_base(type):
-        print("Create the knowledge base for a specified type of knowledge")
+    
+    def create_broad_adverb_knowledge_base(self):
+        # Knowledge base looks like this:
+        # KNOWLEDGE ABOUT ADVERB CATEGORIES
+        # A. CIRCUMSTANCE ADVERBS provide information about...
+        # B. STANCE ADVERBS provide information about ...
+
+        knowledge_base_path = Path(__file__).resolve().parent / "adverbs.json"
+        knowledge_data
+        # Get the knowledge data from the data source
+        try:
+            with knowledge_base_path.open("r", encoding="utf-8") as kb_file:
+                knowledge_data = json.load(kb_file)
+        except FileNotFoundError as exc:
+            raise RuntimeError(f"Knowledge base not found at {knowledge_base_path} at runtime.") from exc
+        
+        # Start building the knowledge base
+        self.knowledge_base = "KNOWLEDGE ABOUT ADVERB CATEGORIES:\n\n"
+
+        # Letter choices
+        letter_choices = ["A", "B", "C", "D"]
+        for idx, (category_name, category_info) in enumerate(knowledge_data["Adverbials"].items()):
+            description = category_info["description"]
+            title = f"{letter_choices[idx]}. {category_name.upper()}"
+            if "adverbs" not in category_name.lower():
+                title += " ADVERBS"
+            self.knowledge_base += f"{title}: {description}\n"
+            self.knowledge_base_mappings[letter_choices[idx]] = {category_name.upper()}
+
+        print(f"Knowledge base prepared: {self.knowledge_base}")
+        return self.knowledge_base
