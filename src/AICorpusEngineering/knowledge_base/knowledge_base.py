87abf1d7a5128433dd77a85159bade576d4c5656
Daniel Parsons <parsonsdwp@gmail.com>
2025-09-23 11:21:58 +0900

    feat: create knowledge base class to manage creation and filtering of knowledge bases

---
+++ src/AICorpusEngineering/knowledge_base/knowledge_base.py
@@ -0,0 +1,11 @@
+
+class KnowledgeBase:
+    """
+    This class manages the creation and filtering of knowledge
+    for injection into the agent templates
+    """
+    def __init__(self):
+        print("Initialize the knowledge base")
+
+    def create_knowledge_base(type):
+        print("Create the knowledge base for a specified type of knowledge")
