6b4b51b5ce4279afbd58b6cbb620bdbfdfdf5edc
Daniel Parsons <parsonsdwp@gmail.com>
2025-09-23 11:29:35 +0900

    feat: create the pipeline class for the ablation study

---
+++ src/AICorpusEngineering/pipelines/ablation_adverbs.py
@@ -0,0 +1,9 @@
+from AICorpusEngineering.logger.logger import NDJSONLogger
+
+class AblationPipeline:
+    """
+    This class controls the classes and data flow for
+    the adverbs ablation study
+    """
+    def __init__(self):
+        print("initialize the class")
